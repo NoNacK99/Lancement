e6e3f34abae8eb5e15f66d8f0f1491fc8ee5e4d1
NoNacK99 <fredericd.physiorob@gmail.com>
2025-06-20 08:24:44 -0400

    Update main.py
    port dynamique

--- main.py
+++ main.py
@@ -426,4 +426,4 @@ async def get_professors():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))  # ✅ Port dynamique
